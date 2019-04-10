308eb7819886b2ade9c077741d64c2485865d39e
orbis25 <orbisalonzo@gmail.com>
2019-04-10 00:03:49 -0400

    init app

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Title extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Places</h1>
+                <p>Descubre lugares de manera simple</p>
+            </div>
+        )
+    }
+}
+
+export default Title; 
